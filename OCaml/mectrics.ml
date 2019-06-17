21eda91fbb3214a752de28c7353e991b4ff73ccb
Aditya Acharya <aacha002@ucr.edu>
2019-06-16 21:30:53 -0700

    letrec functions still not compiling

---
+++ OCaml/mectrics.ml
@@ -0,0 +1,14 @@
+
+
+
+let area x y = x *. y;;
+
+let rec JJ q = 
+        if q < 10 then JJ q+1 else q;;
+
+
+
+let rec Fibb x =
+        if x == 0 then 1 else if x == 1 then 1 else if x > 1 then Fibb(x-1) + Fib(x-2) ;; 
+
+
