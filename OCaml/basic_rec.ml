61f7c384ba3848b4685f74cdb9ffcb0fa572c7b9
Aditya Acharya <30582104+adialachar@users.noreply.github.com>
2019-06-19 20:57:11 -0700

    added database stuff

--- OCaml/basic_rec.ml
+++ OCaml/basic_rec.ml
@@ -5,3 +5,15 @@
     | [] -> [elem]
     | x :: l -> if elem < x then elem :: x :: l
                 else x :: insert elem l;;
+
+
+
+/* database stuff */
+
+# let my_host =
+    let sh cmd = Shell.sh_one_exn cmd in
+    let hostname   = sh "hostname" in
+    let os_name    = sh "uname -s" in
+    let cpu_arch   = sh "uname -p" in
+    let os_release = sh "uname -r" in
+    let timestamp  = Time.now () in
