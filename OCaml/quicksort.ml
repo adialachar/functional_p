eb317fcbbad8b38c068cad58062f4fced1d842a1
Aditya Acharya <30582104+adialachar@users.noreply.github.com>
2019-07-04 22:58:47 -0700

    Update quicksort.ml

--- OCaml/quicksort.ml
+++ OCaml/quicksort.ml
@@ -4,3 +4,12 @@
      let is_less x = x < pivot in
      let left, right = List.partition is_less rest in
      qsort left @ [pivot] @ qsort right
+
+
+ let add_polynom p1 p2 =
+    let n1 = Array.length p1
+    and n2 = Array.length p2 in
+    let result = Array.make (max n1 n2) 0 in
+    for i = 0 to n1 - 1 do result.(i) <- p1.(i) done;
+    for i = 0 to n2 - 1 do result.(i) <- result.(i) + p2.(i) done;
+    result;;
