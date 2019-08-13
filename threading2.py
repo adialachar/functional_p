12450b23cbb41eb2a029a57be0ba44c4e1e925c8
Aditya Acharya <30582104+adialachar@users.noreply.github.com>
2019-08-12 22:10:00 -0700

    Update threading2.py

--- threading2.py
+++ threading2.py
@@ -20,6 +20,33 @@ def calc_quad(numbers):
         time.sleep(0.2)
         print('quad:',n*n*n*n)
 
+
+def calc_cube(numbers):
+    print("calculate cube of numbers")
+    for n in numbers:
+        time.sleep(0.2)
+        print('cube:',n*n*n)
+        
+def calc_quad(numbers):
+    print("calculate quad of numbers")
+    for n in numbers:
+        time.sleep(0.2)
+        print('quad:',n*n*n*n)
+
+
+
+
+arr = [2,3,8,9]
+Arr2 = [5,48,76,88]
+
+t = time.time()
+Now = datetime.datetime.now()
+calc_square(arr)
+calc_cube(arr)
+
+
+
+
 arr = [2,3,8,9]
 Arr2 = [5,48,76,88]
 
