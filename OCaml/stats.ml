9388cb46b03c7188dbfab2d6ebf56ebe92bc45c1
Aditya Acharya <30582104+adialachar@users.noreply.github.com>
2019-08-14 23:01:24 -0700

    Update stats.ml

--- OCaml/stats.ml
+++ OCaml/stats.ml
@@ -1,6 +1,20 @@
 
 
 
+
+let rec averageList L = 
+        match L with
+        | [] -> 0
+        | h::t -> 
+                        let rec sumList lizt = 
+                                match lizt with
+                                | [] -> 0
+                                | h::t -> h +. sumList t
+
+                        in
+                        sumList L /. length L ;; 
+
+
 let rec averageList L = 
         match L with
         | [] -> 0
